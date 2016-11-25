509923d6937e315f33b5e613bbc73c98b28b2eca
Luke Nagy <lukejsnagy@gmail.com>
2016-11-25 23:25:23 +0800

    Setup browserify for client js and include the zepto library.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var browserifyShim = require('browserify-shim');
 var plumber = require('gulp-plumber');
+var source = require('vinyl-source-stream');
+var addsrc = require('gulp-add-src');
 var gutil = require('gulp-util');
 
 var distDir = 'dist';
 
 var handler = function handler(err) {
-  gutil.log(err.stack);
+  gutil.log(err.stack || err);
+  this.emit('end');
 };
 
 gulp.task('server', function () {
@@ -17,19 +23,34 @@ gulp.task('server', function () {
 gulp.task('lib', function () {
   return gulp.src('src/lib/**/*.js', { base: 'src' })
     .pipe(plumber({errorHandler: handler}))
-    .pipe(babel())
+    .pipe(babel({
+      plugins: ['transform-es2015-modules-commonjs']
+    }))
     .pipe(gulp.dest(distDir));
 });
 
 gulp.task('client', function () {
-  return gulp.src('src/client/**/*.html', { base: 'src' })
+  var b = browserify({
+    entries: 'src/client/js/app.js',
+    transform: [
+      babelify.configure({
+        presets: ['es2015']
+      }),
+      browserifyShim
+    ],
+    debug: true
+  });
+
+  return b.bundle().on('error', handler)
+    .pipe(source('client/js/app.js'))
+    .pipe(addsrc(['src/client/**/*.html', 'src/client/js/lib/*.js'], { base: 'src' }))
     .pipe(gulp.dest(distDir));
 });
 
 gulp.task('build', ['server', 'lib', 'client']);
 
 gulp.task('watch', ['build'], function () {
-  gulp.watch('src/**/*.js', ['build']).on('change', function (event) {
+  gulp.watch('src/**', ['build']).on('change', function (event) {
     gutil.log(gutil.colors.blue(event.path + ' ' + event.type));
   });
 });
