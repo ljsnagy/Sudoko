ab4731d2d4990e20307d2167c1efbdb587d15496
Luke Nagy <lukejsnagy@gmail.com>
2016-11-27 12:34:57 +0800

    CSS for the display of the game grid.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var babelify = require('babelify');
 var browserify = require('browserify');
-var browserifyShim = require('browserify-shim');
 var plumber = require('gulp-plumber');
 var source = require('vinyl-source-stream');
 var addsrc = require('gulp-add-src');
+var concat = require('gulp-concat');
 var gutil = require('gulp-util');
 
 var distDir = 'dist';
@@ -29,23 +28,32 @@ gulp.task('lib', function () {
     .pipe(gulp.dest(distDir));
 });
 
-gulp.task('client', function () {
-  var b = browserify({
-    entries: 'src/client/js/app.js',
-    transform: [
-      babelify.configure({
-        presets: ['es2015']
-      }),
-      browserifyShim
-    ],
-    debug: true
-  });
+gulp.task('client-js', function () {
+  var b = browserify({ debug: true })
+    .add('src/client/js/app.js')
+    .transform(require('babelify'), {
+      presets: ['es2015']
+    })
+    .transform(require('browserify-shim'));
 
   return b.bundle().on('error', handler)
     .pipe(source('client/js/app.js'))
-    .pipe(addsrc(['src/client/**/*.html', 'src/client/js/lib/*.js'], { base: 'src' }))
+    .pipe(addsrc('src/client/js/lib/*.js', { base: 'src' }))
     .pipe(gulp.dest(distDir));
 });
+
+gulp.task('client-css', function () {
+  return gulp.src('src/client/css/**.css')
+    .pipe(concat('client/css/app.css'))
+    .pipe(gulp.dest(distDir))
+});
+
+gulp.task('client-static', function () {
+  return gulp.src('src/client/**.!(css|js)', { base: 'src' })
+    .pipe(gulp.dest(distDir))
+});
+
+gulp.task('client', ['client-js', 'client-css', 'client-static']);
 
 gulp.task('build', ['server', 'lib', 'client']);
 
